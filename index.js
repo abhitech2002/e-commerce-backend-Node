4043a319cbfe032f46e31ac5c19f8fad459ed997
abhitech2002 <oabhishek31@gmail.com>
2025-01-25 23:32:04 +0530

    establish MongoDB connection setup and integrate it into the application

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const connectDB = require("./config/db")
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
+
+connectDB();
 
 console.log(`Environment: ${process.env.NODE_ENV}`);
 
