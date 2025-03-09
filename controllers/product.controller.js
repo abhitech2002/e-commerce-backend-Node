41ab11737729f101ef226fdfc9ca464809c41ac6
abhitech2002 <oabhishek31@gmail.com>
2025-03-09 20:16:59 +0530

    add updateProduct route and controller to modify product details

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -37,4 +37,10 @@ const getProductById = asyncHandler(async (req, res) => {
     }
 })
 
-module.exports = { addProduct, getAllProducts, getProductById};
+const updateProduct = asyncHandler(async (req, res) => {
+    const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!updatedProduct) return res.status(404).json({ message: 'Product not found' });
+    res.status(200).json({ success: true, message: 'Product updated successfully', data: updatedProduct });
+})
+
+module.exports = { addProduct, getAllProducts, getProductById, updateProduct};
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { addProduct, getAllProducts, getProductById } = require('../controllers/product.controller.js');
+const { addProduct, getAllProducts, getProductById, updateProduct } = require('../controllers/product.controller.js');
 const { protect, adminOnly } = require('../middlewares/auth.middleware.js');
 
 router.post('/add-product', protect, adminOnly, addProduct);
@@ -8,5 +8,7 @@ router.post('/add-product', protect, adminOnly, addProduct);
 router.get('/get-all-products', getAllProducts);
 
 router.get('/get-product/:id', getProductById);
+
+router.put('/update-product/:id', updateProduct);
 
 module.exports = router;
